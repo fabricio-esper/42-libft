c1107d46150ed68a0c893c7eb9f79363f454c78f
fabricio-esper <fabricio_esper@outlook.com>
2022-05-13 19:58:54 -0300

    Removed ft_strtrim.c and modfied ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 08:08:03 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/05/13 18:14:49 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/05/13 19:57:07 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,39 @@ static int	ft_isspace(int c)
 	return (0);
 }
 
+static int	ft_overflow(int sign)
+{
+	if (sign > 0)
+		return (-1);
+	else
+		return (0);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	long	nbr;
 	long	temp;
 	int		sign;
-	int		i;
 
 	nbr = 0;
 	temp = 0;
 	sign = 1;
-	i = 0;
-	while (ft_isspace(nptr[i]))
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
 	{
-		if (nptr[i] == '-')
-			sign *= -1;
-		i++;
-		if (nptr[i] == '-' || nptr[i] == '+')
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+		if (*nptr == '-' || *nptr == '+')
 			return (0);
 	}
-	while (ft_isdigit(nptr[i]))
+	while (ft_isdigit(*nptr))
 	{
 		temp = nbr;
-		nbr = (nbr * 10) + nptr[i] - 48;
-		if (nbr < temp && sign > 0)
-			return (-1);
-		else if (nbr < temp && sign < 0)
-			return (0);
-		i++;
+		nbr = (nbr * 10) + *nptr++ - 48;
+		if (temp > nbr)
+			return (ft_overflow(sign));
 	}
 	return (sign * nbr);
 }
